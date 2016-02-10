4e8b2a49bf9320533ae8705905986b764a78317c
Kaytee Cannon <kaytee.cannon@mac.com>
2016-02-09 17:07:47 -0700

    End of day

--- TimerViewController.swift
+++ TimerViewController.swift
@@ -16,6 +16,8 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var startButton: UIButton!
     
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,6 +44,107 @@ class TimerViewController: UIViewController {
     @IBAction func pauseButtonTapped(sender: AnyObject) {
     }
     @IBAction func startButtonTapped(sender: AnyObject) {
+        toggleTimer()
+    }
+    
+    func toggleTimer() {
+        if timer.isOn {
+            timer.stopTimer()
+            switchToPickerView()
+        } else {
+            switchToPickerView()
+            
+            let hours = datePicker.countDownDuration / 3600
+            let minutes = (datePicker.countDownDuration - (hours * 3600)) / 60
+            let seconds = (datePicker.countDownDuration - (hours * 3600) - (minutes * 60))
+                
+            
+            timer.setTimer(seconds, totalSeconds: seconds)
+            updateTimerBasedViews()
+            timer.startTimer()
+        }
+    }
+    
+    func updateTimerLabel() {
+        
+        timerLabel.text = timer.string
+    
+    }
+    
+    func updateProgressView() {
+        
+        let secondsElapsed = timer.totalSeconds - timer.seconds
+        
+        let progress = Float(secondsElapsed) / Float(timer.totalSeconds)
+        
+        progressView.setProgress(progress, animated: true)
+    }
+    
+    func updateTimerBasedViews() {
+        updateTimerLabel()
+        updateProgressView()
+    }
+    
+    func timerComplete() {
+        switchToPickerView()
+    }
+    
+    func switchToTimerView() {
+        timerLabel.hidden = false
+        progressView.setProgress(0.0, animated: false)
+        progressView.hidden = false
+        datePicker.hidden = true
+        startButton.setTitle("Cancel", forState: .Normal)
+        
+    }
+    
+    func switchToPickerView() {
+        datePicker.hidden = false
+        timerLabel.hidden = true
+        progressView.hidden = true
+        startButton.setTitle("Start", forState: .Normal)
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
